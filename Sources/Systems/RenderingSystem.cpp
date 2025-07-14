3f1a73861a97e0be599555022545908b3dcf3659
elsantiF <santiagofarall@gmail.com>
2025-07-13 23:03:51 -0300

    fix: adjust rendering rectangle to account for transform scale

--- Sources/Systems/RenderingSystem.cpp
+++ Sources/Systems/RenderingSystem.cpp
@@ -13,8 +13,9 @@ namespace Crane::Systems {
       const auto &transform = view.get<Components::Transform>(entity);
       const auto &renderable = view.get<Components::Renderable>(entity);
 
-      Graphics::Rect rect{transform.position.x - renderable.width / 2.0f, transform.position.y - renderable.height / 2.0f, renderable.width,
-                          renderable.height};
+      Graphics::Rect rect{transform.position.x - renderable.width * transform.scale.x / 2.0f,
+                          transform.position.y - renderable.height * transform.scale.y / 2.0f, renderable.width * transform.scale.x,
+                          renderable.height * transform.scale.y};
 
       renderer.DrawRect(rect, renderable.color);
     }
