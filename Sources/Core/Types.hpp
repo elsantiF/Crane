6c21edaa2430c42274dfa7041cb69f9b5749d303
elsantiF <santiagofarall@gmail.com>
2025-07-26 16:21:25 -0300

    refactor: update numeric type aliases, added Id

--- Sources/Core/Types.hpp
+++ Sources/Core/Types.hpp
@@ -10,15 +10,15 @@
 #include <vector>
 
 // -- Numeric Types --
-using i8 = signed char;
-using i16 = short;
-using i32 = int;
-using i64 = long long;
+using i8 = std::int8_t;
+using i16 = std::int16_t;
+using i32 = std::int32_t;
+using i64 = std::int64_t;
 
-using u8 = unsigned char;
-using u16 = unsigned short;
-using u32 = unsigned int;
-using u64 = unsigned long long;
+using u8 = std::uint8_t;
+using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
 
 using f32 = float;
 using f64 = double;
@@ -79,3 +79,6 @@ Ref<T> MakeRef(Args &&...args) {
 
 template <typename T>
 using WeakRef = std::weak_ptr<T>;
+
+// -- Engine Types --
+using Id = u32;
